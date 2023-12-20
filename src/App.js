147736eb9af8fb2d6df1c49724134effb7337bc3
satak <saigham@mcmaster.ca>
2023-12-19 22:46:40 -0500

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import thunder from './images/icon-memory.svg'
+import memory from './images/icon-reaction.svg'
+import verbal from './images/icon-verbal.svg'
+import visual from './images/icon-visual.svg'
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = 'container'>
+        <div className='firstHalf'>
+            <h1>Your Result</h1>
+            <div className='circle'>
+              <h1>76</h1>
+              <p>of 100</p>
+            </div>
+            <h2>Great</h2>
+            <h5>You scored higher than 65% of the people who have taken these tests.</h5>
+        </div>
+        <div className='secondHalf'>
+          <h3 className = 'headingOne'>Summary</h3>
+          {/* Reaction */}
+          <div className='category reaction'>
+            <div className='reaction'> 
+              <img src = {thunder} alt="thunder"/> <p>Memory</p>
+              </div>
+            <div className='score'>
+              <p className='number'>80 </p>
+              <p className ="hundred"> / 100</p>
+            </div>
+          </div>
+          {/* Memory */}
+          <div className='category memory'>
+            <div className='memory'> 
+              <img src = {memory} alt="memory"/> <p>Memory</p>
+              </div>
+            <div className='score'>
+              <p className='number'>92 </p>
+              <p className ="hundred">/ 100</p>
+            </div>
+          </div>
+          {/* Verbal */}
+          <div className='category verbal'>
+            <div className='verbal'> 
+              <img src = {verbal} alt="verbal"/> <p>Verbal</p>
+              </div>
+            <div className='score'>
+              <p className='number'>61</p>
+              <p className ="hundred">/ 100</p>
+            </div>
+          </div>
+          {/* Visual */}
+          <div className='category visual'>
+            <div className='visual'>
+              <img src = {visual} alt="visual"/> <p>Visual</p>
+            </div>
+            <div className='score'>
+              <p className='number'>72</p>
+              <p className ="hundred">/ 100</p>
+            </div>
+          </div>
+          <div className='button'>
+            <h2>Continue</h2>
+          </div>
+        </div>
+
+      </div>
     </div>
   );
 }
